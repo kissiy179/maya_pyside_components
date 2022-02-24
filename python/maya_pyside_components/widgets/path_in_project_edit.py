ca9cff617bf90d9ffa9ff1dab00c0ec62595a484
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2022-02-24 17:18:24 +0900

    file_path_in_project_editをディレクトリ機能も含んだpath_in_project_editに変更

--- python/maya_pyside_components/widgets/path_in_project_edit.py
+++ python/maya_pyside_components/widgets/path_in_project_edit.py
@@ -3,10 +3,10 @@ import os
 from mayaqt import QtWidgets, QtCore
 import qtawesome as qta
 import maya.cmds as cmds
-from ... import util
-from pyside_components.widgets import file_path_edit, directory_path_edit
+from .. import util
+from pyside_components.widgets import path_edit
 
-class FilePathInMayaProjectEdit(file_path_edit.FilePathEdit):
+class FilePathInMayaProjectEdit(path_edit.FilePathEdit):
     '''
     Mayaプロジェクト内の場合相対パスとして記憶するファイルパス用ウィジェット
     '''
@@ -22,3 +22,17 @@ class FilePathInMayaProjectEdit(file_path_edit.FilePathEdit):
     def setText(self, text):
         text = util.get_relatvie_path_in_maya_project(text)
         super(FilePathInMayaProjectEdit, self).setText(text)
+
+class DirectoryPathInMayaProjectEdit(FilePathInMayaProjectEdit):
+    '''
+    Mayaプロジェクト内の場合相対パスとして記憶するファイルパス用ディレクトリパス用ウィジェット
+    '''
+    open_method = path_edit.getExistingDirectory
+
+class MayaSceneEdit(FilePathInMayaProjectEdit):
+    '''
+    Mayaシーンパス用ウィジェット
+    .ma, .mb, .fbxが有効
+    '''
+    filter = 'Maya scene files (*.ma *.mb);;FBX files (*.fbx)'
+
