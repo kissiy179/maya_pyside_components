86ea6b1cecdf37d0988563a13fdf142c7fdf0012
kishimoto@forces.co.jp <kishimoto@forces.co.jp>
2022-02-28 23:11:13 +0900

    util.get_relatvie_path_in_maya_projectが、存在しないパスを指定した場合に正しい相対パスを返せなかったのを修正

--- python/maya_pyside_components/util/__init__.py
+++ python/maya_pyside_components/util/__init__.py
@@ -3,17 +3,21 @@ import maya.cmds as cmds
 
 def get_relatvie_path_in_maya_project(abs_path):
     pj_path = cmds.workspace(query=True, rootDirectory=True)
+    pj_path = pj_path.replace(os.altsep, os.sep)
+    abs_path = abs_path.replace(os.altsep, os.sep)
+    path_elems = abs_path.split(os.sep)
+    path_elems_len = len(path_elems)
 
-    try:
-        rel_path = os.path.relpath(abs_path, start=pj_path)
+    for i in range(path_elems_len -1):
+        path_elems_ = path_elems[path_elems_len-(i+1):]
+        path_elems_.insert(0, pj_path)
+        rel_path = os.path.join(*path_elems_)
+        
+        if os.path.exists(rel_path):
+            rel_path = rel_path.replace(os.sep, os.altsep)
+            return rel_path
 
-    except:
-        rel_path = abs_path
-
-    if rel_path[:2] == '..':
-        rel_path = abs_path
-
-    return rel_path.replace(os.sep, os.altsep)
+    return abs_path
 
 def get_absolute_path_in_maya_project(rel_path):
     if os.path.isabs(rel_path):
